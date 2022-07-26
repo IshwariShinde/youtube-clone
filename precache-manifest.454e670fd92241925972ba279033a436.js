26b0554e37721bdd98cd5fa4507fff9ba4c22f8d
Ishwari Shinde <ishwarishinde217@gmail.com>
2022-07-26 22:29:54 +0530

    Updates

---
+++ precache-manifest.454e670fd92241925972ba279033a436.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "29696290abbede261353",
+    "url": "/youtube-clone/static/css/main.76f578f6.chunk.css"
+  },
+  {
+    "revision": "29696290abbede261353",
+    "url": "/youtube-clone/static/js/main.29696290.chunk.js"
+  },
+  {
+    "revision": "9ba868c81c09db4f7769",
+    "url": "/youtube-clone/static/js/1.9ba868c8.chunk.js"
+  },
+  {
+    "revision": "dcc2e6b6f9d9b750e6d7",
+    "url": "/youtube-clone/static/js/runtime~main.dcc2e6b6.js"
+  },
+  {
+    "revision": "85295caf4553695062a70cb973179587",
+    "url": "/youtube-clone/static/media/yt1.85295caf.jpeg"
+  },
+  {
+    "revision": "1c586165b84c557f17c7a632573fbe19",
+    "url": "/youtube-clone/static/media/yt2.1c586165.jpeg"
+  },
+  {
+    "revision": "48283e5e502565cf641cebe1f31cf45c",
+    "url": "/youtube-clone/static/media/yt3.48283e5e.jpeg"
+  },
+  {
+    "revision": "ffa612faee643eb13fb54509ca571f50",
+    "url": "/youtube-clone/static/media/food1.ffa612fa.jpg"
+  },
+  {
+    "revision": "0799969a53a3f92b1c4e3b599aef25b7",
+    "url": "/youtube-clone/static/media/logo.0799969a.png"
+  },
+  {
+    "revision": "0faa4455b959ab6d1f57acdf2393c045",
+    "url": "/youtube-clone/static/media/ishayt.0faa4455.jpeg"
+  },
+  {
+    "revision": "b3143a59bc468f171662467d1a3a1808",
+    "url": "/youtube-clone/index.html"
+  }
+];
